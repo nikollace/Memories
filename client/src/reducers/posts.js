e8d620ce623acdf2f9e54e55c5690352010ac077
nikollace <nikoladjordjevicfon@gmail.com>
2021-03-19 12:02:04 +0100

    Serverska strana zavrsena

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,19 +1,22 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes'
+
 const initialState = {
     posts: []
 };
 
 export default (state = [], action) => {
     switch (action.type) {
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             //return {...state, posts: state.posts.concat(action.payload)};
             return [...state, action.payload];
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
+        case LIKE:
             return state.map((post) => (post._id === action.payload._id ? action.payload : post));
-        case 'DELETE':
+        case DELETE:
             return state.filter((post) => post._id !== action.payload);
-        default: return state;
+        default:
+            return state;
     }
 };
--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 
 const app = express();
+
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -15,11 +17,15 @@ app.use(cors());
 //We are adding prefix posts to all router from routes/posts.js
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://nikollace:nana123456@memoriesapp.lgd20.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello to memories API!');
+});
+
+//const CONNECTION_URL = 'mongodb+srv://nikollace:nana123456@memoriesapp.lgd20.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-        .catch((error) => console.log(error.message));
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
